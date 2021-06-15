270e8c7b4e8b32283b6fef7a6d4632887ff1d8f3
Ioane <ioane.natroshvili08b@geolab.edu.ge>
2021-06-16 01:48:26 +0400

    dsd

--- slider.js
+++ slider.js
@@ -3,7 +3,8 @@ jQuery(function () {
     function Slider() {
         var i = 0;
         var x = $('.sliderPic').length;
-
+        $('.sliderPic').eq(i).css({ 'width': `${100}%` });
+        
         //Next Slide
         function ShowPicNext(l) {
             var a = 0;
@@ -51,12 +52,41 @@ jQuery(function () {
 
 
         //Previous Slide
+        function ShowPicPrev(l) {
+            var a = 0;
+
+            var animInterval = setInterval(AnimateShow, 10);
+
+            function AnimateShow() {
+                $('.sliderPic').eq(l).css({
+                    'width': `${a}%`
+                });
+                a++;
+
+                if (a > 100) { clearInterval(animInterval); isClicked = 0;}
+            }
+        }
+
+        function HidePicPrev(l) {
+            var a = 100;
+
+            var animInterval = setInterval(AnimateHide, 10);
+
+            function AnimateHide() {
+                $('.sliderPic').eq(l).css({
+                    'width': `${a}%`
+                });
+                a--;
+
+                if (a < 0) { clearInterval(animInterval); }
+            }
+        }
         function Prev() {
             $('.prev').on('click', function () {
                 i--;
-                ShowPic(i)
-                HidePic(i + 1);
-                if (i <= -1) { i = x - 1; ShowPic(i); HidePic(i + 1); }
+                ShowPicPrev(i)
+                HidePicPrev(i + 1);
+                if (i <= -1) { i = x - 1; ShowPicPrev(i); HidePicPrev(i + 1); }
             });
         }
 
