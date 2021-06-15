3639fdd39134953c0aac913b10b10be6d01eb770
Ioane <ioane.natroshvili08b@geolab.edu.ge>
2021-06-16 01:35:43 +0400

    asdas

--- slider.js
+++ slider.js
@@ -3,21 +3,56 @@ jQuery(function () {
     function Slider() {
         var i = 0;
         var x = $('.sliderPic').length;
-        $(`.sliderPic`).eq(i).css({ 'display': 'block' });
+        var isClicked = 0;
+
+        function ShowPicNext(l) {
+            var a = 0;
+
+            var animInterval = setInterval(AnimateShow, 10);
+
+            function AnimateShow() {
+                $('.sliderPic').eq(l).css({
+                    'width': `${a}%`
+                });
+                a++;
+
+                if (a > 100) { clearInterval(animInterval);}
+            }
+        }
+
+        function HidePicNext(l) {
+            var a = 100;
+
+            var animInterval = setInterval(AnimateHide, 10);
+
+            function AnimateHide() {
+                $('.sliderPic').eq(l).css({
+                    'width': `${a}%`
+                });
+                a--;
+
+                if (a < 0) { clearInterval(animInterval); }
+            }
+        }
+
         function Next() {
-            $('.next').on('click', function () {
-                i++;
-                $(`.sliderPic`).eq(i).css({ 'display': 'block' });
-                $(`.sliderPic`).eq(i - 1).css({ 'display': 'none' });
-                if (i >= x) { i = 0; $(`.sliderPic`).eq(i).css({ 'display': 'block' }); }
-            });
+            console.log(isClicked);
+            if(isClicked == 0) {
+                $('.next').on('click', function () {
+                    i++;
+                    ShowPicNext(i);
+                    HidePicNext(i - 1);
+                    if (i >= x) { i = 0; HidePicNext(i - 1); ShowPicNext(i); }
+                });
+            }
+            isClicked++;
         }
         function Prev() {
             $('.prev').on('click', function () {
                 i--;
-                $(`.sliderPic`).eq(i).css({ 'display': 'block' });
-                $(`.sliderPic`).eq(i + 1).css({ 'display': 'none' });
-                if (i <= -1) { i = x - 1; $(`.sliderPic`).eq(i).css({ 'display': 'block' });}
+                ShowPic(i)
+                HidePic(i + 1);
+                if (i <= -1) { i = x - 1; ShowPic(i); HidePic(i + 1); }
             });
         }
 
