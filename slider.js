2a4a1c5742b29abf6f1e110931f46a7d284e6512
Ioane <ioane.natroshvili08b@geolab.edu.ge>
2021-06-16 00:14:30 +0400

    added functions

--- slider.js
+++ slider.js
@@ -1,5 +1,29 @@
-jQuery(function() {
+jQuery(function () {
 
-    
-    
+    function Slider() {
+        var i = 0;
+        var x = $('.sliderPic').length;
+        $(`.sliderPic`).eq(i).css({ 'display': 'block' });
+        function Next() {
+            $('.next').on('click', function () {
+                i++;
+                $(`.sliderPic`).eq(i).css({ 'display': 'block' });
+                $(`.sliderPic`).eq(i - 1).css({ 'display': 'none' });
+                if (i >= x) { i = 0; $(`.sliderPic`).eq(i).css({ 'display': 'block' }); }
+            });
+        }
+        function Prev() {
+            $('.prev').on('click', function () {
+                i--;
+                $(`.sliderPic`).eq(i).css({ 'display': 'block' });
+                $(`.sliderPic`).eq(i + 1).css({ 'display': 'none' });
+                if (i <= -1) { i = x - 1; $(`.sliderPic`).eq(i).css({ 'display': 'block' });}
+            });
+        }
+
+        Next();
+        Prev();
+    }
+
+    Slider();
 });
