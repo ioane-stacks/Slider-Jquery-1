ecc8fb41c75fc4dd5abe558726f7f1b215e690f3
Ioane <ioane.natroshvili08b@geolab.edu.ge>
2021-06-16 01:41:37 +0400

    Fixed Next button bug

--- slider.js
+++ slider.js
@@ -1,9 +1,9 @@
 jQuery(function () {
-
+    var isClicked = 0;
     function Slider() {
         var i = 0;
         var x = $('.sliderPic').length;
-        var isClicked = 0;
+
 
         function ShowPicNext(l) {
             var a = 0;
@@ -16,7 +16,7 @@ jQuery(function () {
                 });
                 a++;
 
-                if (a > 100) { clearInterval(animInterval);}
+                if (a > 100) { clearInterval(animInterval); isClicked = 0;}
             }
         }
 
@@ -36,17 +36,17 @@ jQuery(function () {
         }
 
         function Next() {
-            console.log(isClicked);
-            if(isClicked == 0) {
-                $('.next').on('click', function () {
+            $('.next').on('click', function () {
+                if (isClicked == 0) {
+                    isClicked++;
                     i++;
                     ShowPicNext(i);
                     HidePicNext(i - 1);
                     if (i >= x) { i = 0; HidePicNext(i - 1); ShowPicNext(i); }
-                });
-            }
-            isClicked++;
+                }
+            });
         }
+
         function Prev() {
             $('.prev').on('click', function () {
                 i--;
